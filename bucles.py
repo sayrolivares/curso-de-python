702c8e1d448ce49e4d5136c955975f52e9d792c5
tkylebjorn <78969699+tkylebjorn@users.noreply.github.com>
2022-02-02 00:47:54 -0500

    Add files via upload

---
+++ bucles.py
@@ -0,0 +1,13 @@
+def run():
+    limite  = 1000000
+
+    contador = 0
+    potencia_2 = 2**contador
+    while potencia_2 < limite:
+        print('2 elevado a ' + str(contador) + ' es igual a: ' + str(potencia_2))
+        contador = contador + 1
+        potencia_2 = 2**contador
+
+
+if __name__ == '__main__':
+    run()
